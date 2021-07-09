0489cf812162d683e72190c90130d4dba40d34bd
Taylor Stephens <stephens.taylora@gmail.com>
2021-07-09 12:49:26 -0400

    can't find error in program wordzz

---
+++ 05_howler/bark.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : Anonymous <Anonymous@localhost>
+Date   : 2021-06-29
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Bark (upper-case input)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='text',
+                        help='Input string or file')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='str',
+                        type=str,
+                        default='')
+
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    out_fh.write(args.text.upper() + '\n')
+    out_fh.close()
+
+
+    
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
